b24b569e798d6b41333c8d981458f1ab0976a740
Jasprit45 <singhsardarjasprit@gmail.com>
2026-02-07 21:33:50 +0530

    Added get tweet with nested comment api

--- src/models/comment.js
+++ src/models/comment.js
@@ -19,7 +19,19 @@ const commentSchema = new mongoose.Schema({
             type:mongoose.Schema.Types.ObjectId,
             refPath:'onModel',
             required:true
-    }
+    },
+    likes: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref:'Like'
+        }
+    ],
+    comments: [
+            {
+                type: mongoose.Schema.Types.ObjectId,
+                ref:'Comment'
+            }
+    ]
 
 }, {timestamps:true});
 
--- src/services/tweet-service.js
+++ src/services/tweet-service.js
@@ -7,26 +7,42 @@ import {TweetRepository,HashtagRepository} from '../repository/index.js';
     }
 
     async create(data) {
-        const content = data.content;
-        const tags = content.match(/#[a-zA-Z0-9_]+/g).map((tag) => tag.substring(1).toLowerCase()); // this regex extract hashtags
-        
-        const tweet = await this.tweetRepository.create(data);
+        try {
+            const content = data.content;
+            const tags = content.match(/#[a-zA-Z0-9_]+/g).map((tag) => tag.substring(1).toLowerCase()); // this regex extract hashtags
+            
+            const tweet = await this.tweetRepository.create(data);
 
-        let alreadyPresentTags = await this.hashtagRepository.getByTitle(tags);
-        let titleOfPresentTags = alreadyPresentTags.map((tag)=> tag.title);
+            let alreadyPresentTags = await this.hashtagRepository.getByTitle(tags);
+            let titleOfPresentTags = alreadyPresentTags.map((tag)=> tag.title);
 
-        let newTags = tags.filter(tag => !titleOfPresentTags.includes(tag));
-        newTags = newTags.map(tag=> {
-            return {title:tag,tweets: [tweet.id]}
-        });
-        await this.hashtagRepository.createMany(newTags);
+            let newTags = tags.filter(tag => !titleOfPresentTags.includes(tag));
+            newTags = newTags.map(tag=> {
+                return {title:tag,tweets: [tweet.id]}
+            });
+            await this.hashtagRepository.createMany(newTags);
 
-        alreadyPresentTags.forEach((tag)=> {
-            tag.tweets.push(tweet.id);
-            tag.save();
-        });
+            alreadyPresentTags.forEach((tag)=> {
+                tag.tweets.push(tweet.id);
+                tag.save();
+            });
+        } catch (error) {
+            console.log('Something went wrong in getting a tweet in tweet repository');
+            throw error;
+        }
 
         return tweet;
+    }
+
+    async get(tweetId) {
+        try {
+            const tweet = await this.tweetRepository.getWithComments(tweetId);
+            return tweet;
+        } catch (error) {
+            console.log('Something went wrong in getting a tweet in tweet repository');
+            throw error;
+        }
+
     }
 }
 
